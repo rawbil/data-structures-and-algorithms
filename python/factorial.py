fd5bb0af56e93891de10c297d05e7a6a2ad7084b
Bil <bildadsimiyu6@gmail.com>
2025-04-14 23:03:31 +0300

    add condition to check if integer is less than 1

--- python/factorial.py
+++ python/factorial.py
@@ -1,7 +1,10 @@
 n = int(input("Enter an integer: "))
 
-factorial = 1
+if(n < 1): 
+    print(f"The factorial of {n} is 1 ")
+else:
+    factorial = 1
 
-for i in range(1, n + 1): # 1, 2, 3, 4, 5
-    factorial = factorial * i
-print(f"The factorial of {n} is: {factorial}")
+    for i in range(1, n + 1): # 1, 2, 3, 4, 5
+        factorial = factorial * i
+    print(f"The factorial of {n} is: {factorial}")
