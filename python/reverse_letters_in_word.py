804b6b32a7f539aa600c93dc44d3268d25fb6c8c
Bil <bildadsimiyu6@gmail.com>
2025-04-14 22:16:59 +0300

    reverse the string functionality

--- python/reverse_letters_in_word.py
+++ python/reverse_letters_in_word.py
@@ -1,1 +1,11 @@
-print "Hello DSA!!"
+n = input("Enter a string: \n")
+
+index = len(n) - 1
+reversed_string = ''
+
+while index >= 0:
+    reversed_string += n[index]
+    index-=1
+
+print(reversed_string)
+
